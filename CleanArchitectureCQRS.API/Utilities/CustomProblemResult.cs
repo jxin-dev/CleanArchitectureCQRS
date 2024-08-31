41fcf6d3b430965f6fe36eddd45d92b9f6aa51c9
Jayson Olicia <oliciajayson@gmail.com>
2024-08-31 10:58:35 +0800

    Add utilities for custom problem result

---
+++ CleanArchitectureCQRS.API/Utilities/CustomProblemResult.cs
@@ -0,0 +1,23 @@
+﻿
+using CleanArchitectureCQRS.SharedKernel.Models;
+using Newtonsoft.Json;
+
+namespace CleanArchitectureCQRS.API.Utilities
+{
+    public class CustomProblemResult : IResult
+    {
+        public Error Error { get; }
+        public CustomProblemResult(Error error)
+        {
+            Error = error;
+        }
+        public async Task ExecuteAsync(HttpContext httpContext)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = Error.StatusCode;
+
+            var response = JsonConvert.SerializeObject(new { code = Error.Code, description = Error.Description });
+            await httpContext.Response.WriteAsync(response);
+        }
+    }
+}
